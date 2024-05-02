efdd63c4b886d869dd3360711076a52de3c6a333
北尾遼馬 <kitaoryoma@kitaoryoubanoMacBook-Air.local>
2024-05-02 18:51:39 +0900

    連射できない

--- Assets/Scripts/RemainImage.cs
+++ Assets/Scripts/RemainImage.cs
@@ -6,34 +6,29 @@ using UnityEngine.UI;
 public class RemainImage : MonoBehaviour
 {
     public Image UIobj;
-    public ShootShell shootshell;
     float remain;
 
-    
 
     // Start is called before the first frame update
     void Start()
     {
-        //ShootShell shootshell = GetComponent<ShootShell>();
-        shootshell = FindObjectOfType<ShootShell>();
-        remain = shootshell.remainShoot;
-        Debug.Log("start");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         // ShootShellクラスのインスタンスを取得する
-        //ShootShell shootshell = GetComponent<ShootShell>();
-
-        // インスタンスのremainShootプロパティにアクセスし、その値を取得する
-        //float remain = shootshell.remainShoot;
-        //remain = ShootShell.remainShoot;
-
-        //Debug.Log("update remain "+remain);
+        ShootShell shootshell = GetComponent<ShootShell>();
+        if (shootshell == null)
+        {
+            Debug.Log("null");
+            return;
+        }
+        remain = shootshell.remainShoot;
 
         // 取得した値を使ってUIobjのfillAmountを設定する
-        //UIobj.fillAmount = remain / 6;
+        UIobj.fillAmount = remain / 6;
     }
 
 
