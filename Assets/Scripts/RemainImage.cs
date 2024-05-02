4ff2d3d1aa008f242a9b7976bab97a17652b29ae
北尾遼馬 <kitaoryoma@kitaoryoubanoMacBook-Air.local>
2024-05-02 17:15:32 +0900

    remain shoot

---
+++ Assets/Scripts/RemainImage.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RemainImage : MonoBehaviour
+{
+    public Image UIobj;
+    float remain =  ShootShell.remainShoot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //ShootShell shootshell = GetComponent<ShootShell>();
+        
+
+        
+        Debug.Log("start remain " + remain);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // ShootShellクラスのインスタンスを取得する
+        //ShootShell shootshell = GetComponent<ShootShell>();
+
+        // インスタンスのremainShootプロパティにアクセスし、その値を取得する
+        //float remain = shootshell.remainShoot;
+        //remain = ShootShell.remainShoot;
+
+        Debug.Log("update remain "+remain);
+        
+        // 取得した値を使ってUIobjのfillAmountを設定する
+        UIobj.fillAmount = remain / 6;
+    }
+
+
+}
--- Assets/Scripts/ShootShell.cs
+++ Assets/Scripts/ShootShell.cs
@@ -8,10 +8,11 @@ public class ShootShell : Photon.Pun.MonoBehaviourPun
     public GameObject shellPrefab;
     //弾の速さを指定
     public float shotSpeed;
+    public float remainShoot;
 
     void Start()
     {
-
+        
     }
 
     void Update()
@@ -20,7 +21,16 @@ public class ShootShell : Photon.Pun.MonoBehaviourPun
         {
             return;
         }
-        if (Input.GetKeyDown(KeyCode.Space))
+
+        //remainShootを増やす　最大6
+        remainShoot += Time.deltaTime;
+        if (remainShoot >= 6)
+        {
+            remainShoot = 6;
+        }
+
+        //弾を打つ
+        if (remainShoot > 0 && Input.GetKeyDown(KeyCode.Space))
         {
             //弾を発生させます
             GameObject shell = Instantiate(shellPrefab, transform.position, Quaternion.identity) as GameObject;
@@ -28,6 +38,9 @@ public class ShootShell : Photon.Pun.MonoBehaviourPun
             //弾に発射の力を加えます
             Rigidbody shellRigidbody = shell.GetComponent<Rigidbody>();
             shellRigidbody.AddForce(transform.forward * shotSpeed);
+
+            //remainShootを減らす
+            remainShoot = remainShoot - 1;
 
             //弾を任意の秒数で消します。
             Destroy(shell, 5.0f);
