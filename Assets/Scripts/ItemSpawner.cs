4c89a6e72215353678911848d764a376845eb92e
松戸満舜 <matsudomitsutoshi@matsudomanshunnoMacBook-Air.local>
2024-05-09 16:22:40 +0900

    pullするため

--- Assets/Scripts/ItemSpawner.cs
+++ Assets/Scripts/ItemSpawner.cs
@@ -8,6 +8,7 @@ public class ItemSpawner : MonoBehaviourPunCallbacks
     public GameObject itemPrefab; // アイテムのプレファブ
     public float spawnInterval = 10f; // アイテムが生成される時間間隔（秒）
     private float timer;
+    private GameObject item;
 
     void Update()
     {
@@ -24,12 +25,25 @@ public class ItemSpawner : MonoBehaviourPunCallbacks
             SpawnItem();
             timer = 0;
         }
+
     }
+
+    // 当たった時に呼ばれる関数
+    void OnCollisionEnter(Collision collision)
+    {
+        PhotonNetwork.Destroy(item);
+        Debug.Log("item_collision");
+
+    }
+
+
 
     void SpawnItem()
     {
         Vector3 spawnPosition = new Vector3(Random.Range(24f, 25f), 2, Random.Range(-10f, -9f));
-        PhotonNetwork.Instantiate(itemPrefab.name, spawnPosition, Quaternion.identity);
+        item = PhotonNetwork.Instantiate(itemPrefab.name, spawnPosition, Quaternion.identity) as GameObject;
         itemPrefab.SetActive(true);
+
+        
     }
 }
