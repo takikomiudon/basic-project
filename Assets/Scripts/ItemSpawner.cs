9bf8a48c6679991b49d887510c7033ca748bbc12
松戸満舜 <matsudomitsutoshi@matsudomanshunnoMacBook-Air.local>
2024-05-09 16:26:53 +0900

    pull

--- Assets/Scripts/ItemSpawner.cs
+++ Assets/Scripts/ItemSpawner.cs
@@ -28,22 +28,39 @@ public class ItemSpawner : MonoBehaviourPunCallbacks
 
     }
 
-    // 当たった時に呼ばれる関数
-    void OnCollisionEnter(Collision collision)
+
+
+    //  アイテムの発生場所をランダムに生成。
+    Vector3 GenerateSpawnPosition()
     {
-        PhotonNetwork.Destroy(item);
-        Debug.Log("item_collision");
+        float x = Random.Range(0, 200); // 0 から 200 までのランダムな値を生成
+        Vector3 position;
 
+        if (x < 55)
+        {
+            position = new Vector3(x - 33, 1, -13);
+        }
+        else if (x < 100)
+        {
+            position = new Vector3(22, 1, x - 68);
+        }
+        else if (x < 155)
+        {
+            position = new Vector3(x - 133, 1, 32);
+        }
+        else // x is between 155 and 200
+        {
+            position = new Vector3(-33, 1, x - 168);
+        }
+
+        return position;
     }
-
-
 
     void SpawnItem()
     {
-        Vector3 spawnPosition = new Vector3(Random.Range(24f, 25f), 2, Random.Range(-10f, -9f));
-        item = PhotonNetwork.Instantiate(itemPrefab.name, spawnPosition, Quaternion.identity) as GameObject;
+        Vector3 spawnPosition = GenerateSpawnPosition(); // ランダムなスポーン位置を取得
+        Quaternion rotation = Quaternion.Euler(45, 0, 0);  // 角度調節
+        PhotonNetwork.Instantiate(itemPrefab.name, spawnPosition, rotation);
         itemPrefab.SetActive(true);
-
-        
     }
 }
